08ae9e64f79d690c5b7ac3d047a23a28eed47c4c
king <kingbian72@gmail.com>
2023-11-06 22:34:43 -0500

    solved

---
+++ cpp/1365_how_many_nums.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+vector<int> smallerThanCurrent(vector<int>& nums){
+    vector<int> ans; 
+    
+
+    for(int i=0; i<nums.size(); ++i){
+        int count =0; 
+        for( int j=0; j<nums.size(); ++j){
+            
+            if(i !=j && nums[j]<nums[i]){
+                count++;
+            }
+
+        }
+        ans.push_back(count); 
+    }
+
+    return ans; 
+}
+
+
+int main(){
+
+    vector<int> nums{ 8, 1, 2, 2, 3 };
+
+    for(int i: smallerThanCurrent(nums)){
+        cout<<i;
+    }
+    cout<<endl;
+}
