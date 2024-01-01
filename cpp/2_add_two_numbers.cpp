81a08f347aec68d065121e30545ac814976b19e4
king <kingbian72@gmail.com>
2024-01-01 15:18:29 -0500

    solved but very slow **needs updating**

--- cpp/2_add_two_numbers.cpp
+++ cpp/2_add_two_numbers.cpp
@@ -9,26 +9,30 @@ struct ListNode
     ListNode(int x, ListNode *next); // val = x. next = next node;
 };
 
+/**
+ * Run time 61ms very slow needs to be updated
+ */
+
 ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
 
     int carry = 0; // if the sum is > 10
-    ListNode *ans = new ListNode(0);
+    ListNode *ans = new ListNode();
 
     ListNode *current = ans;
 
-    while (l1 != nullptr || l2 != nullptr || carry != 0)
+    while (l1 != NULL || l2 != NULL || carry != 0)
     {
         int val1, val2, sum;
         val1 = val2 = sum = 0;
 
-        if (l1->next != nullptr)
+        if (l1->next != NULL)
         {
             val1 = l1->val;
             l1 = l1->next;
         }
 
-        if (l2->next != nullptr)
+        if (l2->next != NULL)
         {
             val2 = l2->val;
             l2 = l2->next;
@@ -36,43 +40,12 @@ ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 
         sum = val1 + val2 + carry;
 
-        ans->next = new ListNode(sum % 10);
+        ListNode *tempNode = new ListNode(sum % 10);
 
+        current->next = tempNode;
+        current = current->next;
         carry = sum / 10;
     }
 
-    return ans;
-}
-
-int main()
-{
-
-    ListNode *l1 = nullptr;
-    ListNode *l2 = nullptr;
-
-    std::string node1;
-    std::cin >> node1;
-
-    // std::string node2;
-    // std::cin >> node2;
-
-    std::cout << "Node 1 data: " << node1 << "\n";
-    // std::cout << "Node 2 data: " << node2 << "\n";
-
-    for (int i = 0; i < node1.size(); ++i)
-    {
-
-        int val = node1[i] - '0';
-
-        if (l1->next == nullptr)
-        {
-
-            l1 = new ListNode(val);
-        }
-        else
-        {
-            l1->next = new ListNode(val);
-            l1 = l1->next;
-        }
-    }
+    return ans->next;
 }
