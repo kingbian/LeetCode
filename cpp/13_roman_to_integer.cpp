edd0f9da658937e31ec50c1a8330d2b47861b7b1
king <kingbian72@gmail.com>
2024-01-02 20:53:50 -0500

    clean up

--- cpp/13_roman_to_integer.cpp
+++ cpp/13_roman_to_integer.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-
+#include <chrono>
+#include <cstdlib>
 using namespace std;
 
 int romans(string s)
@@ -16,41 +17,26 @@ int romans(string s)
         {'D', 500},
         {'M', 1000}};
 
-    // auto printPrintKeyValue = [](const char& key, const int& value){
-
-    //     std::cout<<"key: "<<key<<" value: "<<value<<"\n";
-    // };
-
-    // std::cout<<"printing unordered map\n";
-    // for(const auto& [key, value] : map){
-    //     printPrintKeyValue(key, value);
-    // }
-
     int ans = 0;
     int perviousVal = 0;
     for (int i = 0; i < s.length(); ++i)
     {
 
         char element = s[i];
-        // std::cout << element;
 
         int currentVal = map.find(element)->second;
 
         if (perviousVal < currentVal)
         {
             int newVal = currentVal - perviousVal;
-            // std::cout << "current value for element : " << element << " newVal: " << newVal << "\n";
             ans += newVal - perviousVal;
         }
         else
         {
-            // std::cout << "current value for element : " << element << " value: " << currentVal << "\n";
             ans += currentVal;
         }
         perviousVal = currentVal;
     }
-
-    // std::cout << ans << "\n";
 
     return ans;
 }
